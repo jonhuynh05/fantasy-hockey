2f40cf8dc0ccc5ee3e2d1fd823e18656cd5695f9
Jonathan Huynh <jonhuynh05@gmail.com>
2020-04-09 14:26:43 -0700

    handle state change for admin

---
+++ src/Admin/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import {withRouter} from "react-router-dom"
+
+class Admin extends Component{
+    render(){
+        return(
+            <div>
+                <div>
+                    Are you the commish? Log in below.
+                </div>
+                <div>
+                    <form>
+                        <input onChange={this.props.handleChange} placeholder="Username" name="username"></input>
+                        <input onChange={this.props.handleChange}  placeholder="Password" name="password" type="password"></input>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default withRouter(Admin)
--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import {withRouter, Switch, Route} from "react-router-dom"
 import './App.css';
 import Home from "./Home"
+import Admin from "./Admin"
 
 class App extends Component {
+  state={
+    username: "",
+    password: ""
+  }
+
+  handleChange =(e) => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value
+    })
+  }
+
   render(){
     return(
       <div>
         <Switch>
           <Route exact path={"/"} render={() => <Home />}/>
+          <Route exact path={"/thecommissioner"} render={() => <Admin username={this.state.username} password={this.state.password} handleChange={this.handleChange}/>}/>
         </Switch>
       </div>
     )
