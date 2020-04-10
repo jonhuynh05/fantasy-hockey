30298644e21c3960d4e998674ae00ee60aa611c1
Jonathan Huynh <jonhuynh05@gmail.com>
2020-04-10 11:12:49 -0700

    some styling

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
+import "./home.css"
 
 class Home extends Component{
     render(){
         return(
-            <div>
-                <div>
+            <div id="home-container">
+                <div id="champion-header">
                     All Hail 2018 Champion, Evil Empire!
                 </div>
                 <div>
