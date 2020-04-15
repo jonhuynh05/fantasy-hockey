aea5dcb5ceaabad4cf567c3dc2701a22d98a6f13
Jonathan Huynh <jonhuynh05@gmail.com>
2020-04-15 11:00:22 -0700

    add window size listener

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -3,35 +3,61 @@ import { withRouter, Link } from 'react-router-dom'
 import "./navbar.css"
 
 class NavBar extends Component{
+
+    state = {
+        width: window.innerWidth
+    }
+
+    updateWindowSize = () => {
+        this.setState({
+            width: window.innerWidth
+        })
+    }
+
+    async componentDidMount(){
+        window.addEventListener("resize", this.updateWindowSize)
+    }
+
     render(){
         return(
             <div id="navbar-container">
-                <Link to="/" className="nav-link" id="home-link">
-                    <div className="link-tab">
-                        League of Leagues
+                {
+                    this.state.width < 720
+                    ?
+                    <div id="hamburger-stack" onClick={this.handleClose}>
+                        <div className="hamburger-bars"></div>
+                        <div className="hamburger-bars"></div>
+                        <div className="hamburger-bars"></div>
                     </div>
-                </Link>
-                <Link to="/about" className="nav-link" id="about-link">
-                    <div className="link-tab">
-                        About
-                    </div>
-                </Link>
-                <Link to="/champions" className="nav-link" id="champions-link">
-                    <div className="link-tab">
-                            Champions
-                    </div>
-                </Link>
-                <Link to="/trade-history" className="nav-link" id="trade-link">
-                    <div className="link-tab">
-                            Trade History
-                    </div>
-                </Link>
-                <Link to="/draft-history" className="nav-link" id="draft-link">
-                    <div className="link-tab">
-                            Draft History
-                    </div>
-                </Link>
-
+                    :
+                    <>
+                        <Link to="/" className="nav-link" id="home-link">
+                        <div className="link-tab">
+                            League of Leagues
+                        </div>
+                        </Link>
+                        <Link to="/about" className="nav-link" id="about-link">
+                            <div className="link-tab">
+                                About
+                            </div>
+                        </Link>
+                        <Link to="/champions" className="nav-link" id="champions-link">
+                            <div className="link-tab">
+                                    Champions
+                            </div>
+                        </Link>
+                        <Link to="/trade-history" className="nav-link" id="trade-link">
+                            <div className="link-tab">
+                                    Trade History
+                            </div>
+                        </Link>
+                        <Link to="/draft-history" className="nav-link" id="draft-link">
+                            <div className="link-tab">
+                                    Draft History
+                            </div>
+                        </Link>
+                    </>
+                }
             </div>
         )
     }
