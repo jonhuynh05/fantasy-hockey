75c81c6e6e7b010760f74eb1181605f2a1ae8b5d
Jonathan Huynh <jonhuynh05@gmail.com>
2020-04-17 11:55:34 -0700

    update mobile nav position

--- src/Draft-Year/index.js
+++ src/Draft-Year/index.js
@@ -5,7 +5,12 @@ import "./draftyear.css"
 class DraftYear extends Component{
 
     state = {
-        draftDetails: []
+        draftDetails: [],
+        year: "",
+        round: "",
+        pick: "",
+        team: "",
+        selection: ""
     }
 
     async getDraftDetails (){
@@ -46,6 +51,37 @@ class DraftYear extends Component{
             }
           })
         this.getDraftDetails()
+    }
+
+
+    handleChange = (e) => {
+        this.setState({
+            [e.currentTarget.name]: e.currentTarget.value
+        })
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault()
+        await fetch(`/drafts/new`, {
+            method: "POST",
+            credentials: "include",
+            body: JSON.stringify(this.state),
+            headers: {
+              "Content-Type": "application/json"
+            }
+        })
+        .then(async res => {
+            const response = await res.json()
+            console.log(response)
+            this.setState({
+                year: "",
+                round: "",
+                pick: "",
+                team: "",
+                selection: ""
+            })
+            this.getDraftDetails()
+        })
     }
 
     render(){
@@ -116,11 +152,32 @@ class DraftYear extends Component{
                     }
                 </div>
                 {draftDetails}
+
+
                 {
                     this.props.isLoggedIn
                     ?
-                    <div className="category" id="remove-button-container">
-                        <button onClick={this.handleDeleteYear} id="remove-button">Remove Draft</button>
+                    <div className="category-input-row">
+                        <form id="category-input-form">
+                            <input className="category-input" id="round-input" placeholder="Round" name="round" value={this.state.round} onChange={this.handleChange}/>
+                            <input className="category-input" id="pick-input" placeholder="Pick" name="pick" value={this.state.pick} onChange={this.handleChange} required/>
+                            <input className="category-input" id="team-input" placeholder="Team" name="team" value={this.state.team} onChange={this.handleChange}/>
+                            <input className="category-input" id="selection-input" placeholder="Selection" name="selection" value={this.state.selection} onChange={this.handleChange}/>
+                            <input className="category-input" id="year-input" placeholder="Year" name="year" value={this.state.year} onChange={this.handleChange} required/>
+                            <button onClick={this.handleSubmit}id="draft-submit-button">Submit</button>
+                        </form>
+                    </div>
+                    :
+                    null
+                }
+
+
+
+                {
+                    this.props.isLoggedIn
+                    ?
+                    <div className="category" id="remove-draft">
+                        <button onClick={this.handleDeleteYear} className="remove-button">Remove Draft</button>
                     </div>
                     :
                     null
