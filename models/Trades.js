9f13937dfde7e8a498b05e9ba4a5316d8130ba8c
Jonathan Huynh <jonhuynh05@gmail.com>
2020-04-22 10:28:40 -0700

    being update trade model

--- models/Trades.js
+++ models/Trades.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 const tradesSchema = new Schema ({
+    number: String,
     team1: String,
     team2: String,
     acquisition1: String,
